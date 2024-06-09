f4f656fafe0d6e8f74a6987e801af44be458afcd
Thiago Lins <thiagolins00@gmail.com>
2024-06-08 23:55:06 -0300

    Salvando o arquivo no versionamento.

---
+++ proj_inicial/main.py
@@ -0,0 +1,19 @@
+print("Jornada de dados")
+
+# Variáveis
+idade = 29
+nome = "Thiago"
+
+# Print
+print(idade)
+
+# Soma
+nova_idade = idade + 2
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
+
